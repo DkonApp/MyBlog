834d94b00796e60737e58ee9f39b1d8438ef8ea3
Aan Saari <169133600+DalamKontak@users.noreply.github.com>
2024-10-15 13:46:16 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+const siteConfig = {
+    title: "My Person Blog",
+    description: "This is my personal blog where I share my thoughts and photos.",
+    headertitle: "My Person Blog",
+    keywords: "blog, personal, photos, thoughts",
+    colorPalette: {
+        primary: "#4CAF50", //The main color
+        secondary: "#f4f4f4", // Secondary color
+        textColor: "#333", // Text color
+        backgroundColor: "#ffffff" // Background color
+    },
+    galleryPosition: "bottom" // "top" and "bottom"
+};
